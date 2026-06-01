cfd69c2788386d6647cf068ba8e957cbe4df17f1
Jacinta2503 <2425402503@live.gctu.edu.gh>
2026-06-01 12:47:34 +0000

    Initial Assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+#File:JNP.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Jacinta Naane Naaoyo Quartey-Papafio
+# STUDENT ID: 2425402503
+# Email: 2425402503@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Jacinta Naane Naaoyo Quartey-Papafio, this programming
+# assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
   pass #code goes here
 
-
+    print()
+    print("...JNP")
+    print()
+    print("...JJJJJJJJJJJJ........NNNN....NNNN........PPPPPPPPPPPP.......")
+    print("...JJJJJJJJJJJJ........NNNNN...NNNN........PPPPPPPPPPPPP......")
+    print("...........JJJJ........NNNNNN..NNNN........PPPP......PPPP.....")
+    print("...........JJJJ........NNNN.NN.NNNN........PPPP......PPPP.....")
+    print("...........JJJJ........NNNN..NNNNNN........PPPPPPPPPPPPP......")
+    print("...........JJJJ........NNNN...NNNNN........PPPPPPPPPPPP.......")
+    print("...JJ......JJJJ........NNNN....NNNN........PPPP...............")
+    print("...JJJ.....JJJJ........NNNN....NNNN........PPPP...............")
+    print(".....JJJJJJJJJJ...**...NNNN....NNNN...**...PPPP.............**")
+    print("........JJJJJJJ...**...NNNN....NNNN...**...PPPP.............**")
+    print()
 
 main()
